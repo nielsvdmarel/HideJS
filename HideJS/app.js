1d0eaa00c808b4539835f3d954e8c7a6b4e447dc
NabilCherigui <mrtopgamer@hotmail.com>
2017-06-29 13:39:40 +0200

    thing

--- HideJS/app.js
+++ HideJS/app.js
@@ -8,55 +8,37 @@ app.use(express.static('public'));
 serv.listen(servPort);
 console.log("Server started on port " + servPort);
  
-let SOCKET_LIST = {};
-let PLAYER_LIST = {};
- 
-let Player = id => {
-    let self = {
-        x:0,
-        y:0,
-        id:id,
-        pressingRight:false,
-        pressingLeft:false,
-        pressingUp:false,
-        pressingDown:false,
-        maxSpd:10,
-    }
-    self.updatePosition = () => {
-        if(self.pressingRight)
-            self.x += self.maxSpd;
-        if(self.pressingLeft)
-            self.x -= self.maxSpd;
-        if(self.pressingUp)
-            self.y -= self.maxSpd;
-        if(self.pressingDown)
-            self.y += self.maxSpd;
-    }
-    return self;
-}
+let SOCKET_LIST = [];
+let PLAYER_LIST = [];
+
  
 let io = require('socket.io')(serv,{});
 io.sockets.on('connection', (socket) => {
     socket.id = Math.random();
     SOCKET_LIST[socket.id] = socket;
- 
-    let player = Player(socket.id);
-    PLAYER_LIST[socket.id] = player;
-   
+  
     socket.on('disconnect',() => {
         delete SOCKET_LIST[socket.id];
         delete PLAYER_LIST[socket.id];
     });
+    
+    socket.on('joined', data => {
+        if(data.input){
+            socket.broadcast.emit("player join", {
+                id: socket.id
+            })
+            for(var i = 0; i < SOCKET_LIST.length; i++){
+                socket.emit("player join", {id: SOCKET_LIST[i].id});
+            }
+        }
+        //socket.emit()
+    });
    
-    socket.on('keyPress',(data) => {
-        if(data.inputId === 'left')
-            player.pressingLeft = data.state;
-        else if(data.inputId === 'right')
-            player.pressingRight = data.state;
-        else if(data.inputId === 'up')
-            player.pressingUp = data.state;
-        else if(data.inputId === 'down')
-            player.pressingDown = data.state;
+    socket.on('update',(data) => {
+        socket.broadcast.emit("playerMove", {
+            id:socket.id,
+            position:data.position
+        })
     });
    
    
